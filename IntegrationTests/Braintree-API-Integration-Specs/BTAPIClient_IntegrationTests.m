c4224dabc8e3e9ed388f892c6d531e0100d1421d
scannillo <samantha.cannillo@getbraintree.com>
2020-01-09 13:37:55 -0600

    Add integration test for PP UAT
    Co-authored-by: hstotelmyer <holly.stotelmyer@getbraintree.com>
    Co-authored-by: scannillo <samantha.cannillo@getbraintree.com>

--- IntegrationTests/Braintree-API-Integration-Specs/BTAPIClient_IntegrationTests.m
+++ IntegrationTests/Braintree-API-Integration-Specs/BTAPIClient_IntegrationTests.m
@@ -50,4 +50,18 @@
     [self waitForExpectationsWithTimeout:5 handler:nil];
 }
 
+- (void)pendFetchConfiguration_withPayPalUAT_returnsTheConfiguration {
+    // NOTE: - This UAT will expire. To run this test, you must generate a fresh UAT.
+
+    BTAPIClient *client = [[BTAPIClient alloc] initWithAuthorization:@"eyJraWQiOiI1NTY1MWVhZWE0MjZjZDVhMjM5ZWU0ZjUwMTczMDk3NmI2YzMxZmNkIiwidHlwIjoiSldUIiwiYWxnIjoiRVMyNTYifQ.eyJpc3MiOiJodHRwczovL2FwaS5wYXlwYWwuY29tIiwic3ViIjoiUGF5UGFsOlc1Skw2VlVKVTlUTUwiLCJhY3IiOlsiY2xpZW50Il0sIm9wdGlvbnMiOnt9LCJheiI6Im1zbWFzdGVyMWludC1nX2lkZW50aXR5c2VjdXJldG9rZW5zZXJ2XzEucWEiLCJzY29wZXMiOlsiQnJhaW50cmVlOlZhdWx0Il0sImV4cCI6MTU3ODYzMDE1MywiZXh0ZXJuYWxfaWRzIjpbIlBheVBhbDpXNUpMNlZVSlU5VE1MIiwiQnJhaW50cmVlOm15bWt5Mm12eDVteWo1eXoiXSwianRpIjoiVTJBQUhhenpIQm1NU3Z0RXhEM1ZIak5PWWZaSFBKOHdTaGVVNEM1NFg1cTVMSnNQcTBvRVJneUVMM0FHRmlsUC1vMDhfQ0lnbzlSS3dqSUw4MzZoT3ZlZkVjcGtIWXMzZUh0OE5KYVhYZmdvZzJJNmk1Nmp2QW5zdlhNSUtlY3cifQ.GXhER7Ttn7QIqZ8Zq4Zj7iV6qyFbDddcemS9id2VyJidBgZWZZgg5Je8p7LUXcBfsjZrWaBYSepERTIEHSXHzg"];
+
+    XCTestExpectation *expectation = [self expectationWithDescription:@"Fetch configuration"];
+    [client fetchOrReturnRemoteConfiguration:^(BTConfiguration *configuration, NSError *error) {
+        XCTAssertEqualObjects([configuration.json[@"merchantId"] asString], @"mymky2mvx5myj5yz");
+        XCTAssertNil(error);
+        [expectation fulfill];
+    }];
+
+    [self waitForExpectationsWithTimeout:5 handler:nil];
+}
 @end
